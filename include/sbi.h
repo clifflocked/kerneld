4b53a41ef36c7c71b05f9df845c82bea305ba956
Segen Stoutamire <segen@stoutamire.com>
2026-03-09 16:19:14 -0700

    move some headers around

--- include/sbi.h
+++ include/sbi.h
@@ -1,6 +1,15 @@
 #ifndef _SBI_H
 #define _SBI_H
 
+struct sbiret  {
+    long error;
+    long value
+};
+
+struct sbiret sbi_ecall(long a0, long a1, long a2,
+                        long a3, long a4, long a5,
+                        long fid, long eid);
+
 long sbi_console_putchar(int ch);
 
 [[noreturn]]
